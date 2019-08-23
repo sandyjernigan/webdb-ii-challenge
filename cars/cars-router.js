61809ffd689b4aaa6dfbe895b295fbe8c3e91952
Sandy <sandy.jernigan@gmail.com>
2019-08-23 00:30:07 -0400

    Read all, read by id, and create endpoints are working

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -27,7 +27,7 @@ router.get('/:id', (req, res) => {
     res.json(results);
   }) 
   .catch (err => {
-    res.status(500).json({ message: 'Failed to retrieve fruit' });
+    res.status(500).json({ message: 'Failed to retrieve the car by id' });
   });
 });
 
